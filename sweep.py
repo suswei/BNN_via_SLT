a3d11fe0aad5174191c7244ccf742e397a95ed74
Susan Wei <stawbygl@gmail.com>
2021-04-08 16:20:30 +1000

    prior var gaussian experiment

--- sweep.py
+++ sweep.py
@@ -10,8 +10,9 @@ def set_sweep_config():
     rr_Hs = [80, 90, 100]
     ns = [5000]
     seeds = [1]
-    rr_ls = [3000, 4000, 5000]
-    beta_modes = ['lmbda_star','ones']
+    rr_ls = [10, 100, 500]
+    beta_modes = ['lmbda_star']
+    priorvars = [1, 1e-2, 1e-4]
 
     sweep_params = {'rr_Hs': rr_Hs, 'tanh_Hs': tanh_Hs, 'seeds': seeds}
     hyperparameter_experiments = []
@@ -38,6 +39,7 @@ def set_sweep_config():
         'dataset': ['reducedrank'],
         'var_mode': ['nf_gamma'],
         'n': ns,
+        'prior_var': priorvars,
         'lmbda_star': rr_ls,
         'beta_mode': beta_modes,
     }
@@ -51,6 +53,7 @@ def set_sweep_config():
         'dataset': ['reducedrank'],
         'var_mode': ['nf_gaussian'],
         'n': ns,
+        'prior_var': priorvars,
         'lmbda_star': [20], #NA
         'beta_mode': ['ones'] #NA
     }
@@ -67,14 +70,15 @@ def main(taskid):
     temp = hyperparameter_experiments[taskid]
 
     if temp['var_mode'] == 'nf_gaussian':
-        path = '{}_{}_n{}_H{}_seed{}'.format(temp['var_mode'], temp['dataset'], temp['n'], temp['H'],temp['seed'])
+        path = '{}_{}_n{}_H{}_seed{}_priorvar{}'.format(temp['var_mode'], temp['dataset'], temp['n'], temp['H'],temp['seed'], temp['prior_var'])
     else:
-        path = '{}_{}_n{}_H{}_seed{}_l{}_betamode{}'.format(temp['var_mode'], temp['dataset'], temp['n'], temp['H'],temp['seed'],temp['lmbda_star'],temp['beta_mode'])
+        path = '{}_{}_n{}_H{}_seed{}_l{}_betamode{}_priorvar{}'.format(temp['var_mode'], temp['dataset'], temp['n'], temp['H'],temp['seed'],temp['lmbda_star'],temp['beta_mode'], temp['prior_var'])
 
     os.system("python3 main.py "
               "--dataset %s "
-              "--epochs 3000 "
-              "--prior gmm "
+              "--epochs 5000 "
+              "--prior gaussian "
+              "--prior_var %s "
               "--sample_size %s "
               "--seed %s "
               "--H %s "
@@ -82,7 +86,7 @@ def main(taskid):
               "--beta_mode %s "
               "--lmbda_star %s "
               "--path %s"
-              % (temp['dataset'], temp['n'], temp['seed'], temp['H'], temp['var_mode'], temp['beta_mode'], temp['lmbda_star'], path))
+              % (temp['dataset'], temp['prior_var'], temp['n'], temp['seed'], temp['H'], temp['var_mode'], temp['beta_mode'], temp['lmbda_star'], path))
 
 
 if __name__ == "__main__":
