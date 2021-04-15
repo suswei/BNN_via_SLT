fixed gammatrunc entropy calculation

main.py
@@ -17,6 +17,7 @@ def train(args):
         resolution_network = RealNVP(dim=args.w_dim, hidden_dim=args.nf_hidden, layers=args.nf_layers, af=args.nf_af)
     elif args.nf == 'vanilla_rnvp':
         resolution_network = R_NVP(d=args.w_dim, k=args.w_dim//2, hidden=args.nf_hidden, layers=args.nf_layers)
+
     optimizer = torch.optim.Adam(resolution_network.parameters(), lr=args.lr)
     scheduler = custom_lr_scheduler.CustomReduceLROnPlateau\
         (optimizer, 'min', verbose=True, factor=0.9, patience=100, eps=1e-6)
@@ -37,11 +38,10 @@ def train(args):
             args.theta_lower = torch.min(thetas, dim=0).values.detach() #need to do by dim, see gengamma_uniform branch
             args.theta_upper = torch.max(thetas, dim=0).values.detach()
 
-            # transformed_prior = torch.exp(log_prior(args,thetas)+log_jacobians) # check this is positive
-
-            loglik_elbo_vec = loglik(thetas, data, target, args)  # E_q \sum_i=1^m p(y_i |x_i , g(\xi))
+            loglik_elbo_vec = loglik(thetas, data, target, args)  # [R, minibatch_size] E_q \sum_i=1^m p(y_i |x_i , g(\xi))
 
             complexity = - log_prior(args, thetas).mean() - log_jacobians.mean()  # q_entropy no optimization
+
             if args.blundell_weighting:
                 M = args.sample_size/args.batch_size # number of minibatches
                 pi = (2**(M-batch_idx))/(2**M-1) # follows blundell, is bad for nf_gammatrunc
@@ -81,29 +81,16 @@ def evaluate(resolution_network, args, R):
         xis = sample_q(args, R, exact=True)  # [R, args.w_dim]
         thetas, log_jacobians = resolution_network(xis)  # [R, args.w_dim], [R]
 
-        # transformed_prior = torch.exp(log_prior(args, thetas) + log_jacobians)  # check this is positive
-        # print('transformed prior at xi {}'.format(transformed_prior))
-
         print('thetas min {} max {}'.format(thetas.min(), thetas.max()))
         print('xis min {} max {}'.format(xis.min(), xis.max()))
 
-        # assuming xis \in [0,1]^d
-        # theta1 = xis[:,0]
-        # theta2 = 1.62167 / ((np.sqrt(2) * xis[:,1]) ** (-1) - 0.405963)
-        #
-        # plt.plot(xis[:, 1], xis[:, 2],'.')
-        # plt.plot(thetas[:, 1], thetas[:, 2],'r.')
-        # plt.plot(theta1,theta2,'.')
-        # plt.show()
-
-        args.theta_lower = torch.min(thetas, dim=0).values.detach()  # need to do by dim, see gengamma_uniform branch
+        args.theta_lower = torch.min(thetas, dim=0).values.detach()
         args.theta_upper = torch.max(thetas, dim=0).values.detach()
-        logprior = log_prior(args, thetas)  # [R]
 
-        args.xi_upper = xis.max()
+        args.xi_upper = torch.max(xis, dim=0).values.detach()
         ent = q_entropy_sample(args, xis)
 
-        complexity = ent - logprior.mean() - log_jacobians.mean()
+        complexity = ent - log_prior(args, thetas).mean() - log_jacobians.mean()
 
         elbo_loglik = 0.0
         for batch_idx, (data, target) in enumerate(args.train_loader):
@@ -115,7 +102,7 @@ def evaluate(resolution_network, args, R):
         for batch_idx, (data, target) in enumerate(args.val_loader):
             elbo_loglik_val += loglik(thetas, data, target, args).sum(dim=1)
 
-    return elbo, elbo_loglik.mean(), complexity, ent, logprior.mean(), log_jacobians.mean(), elbo_loglik_val.mean()
+    return elbo, elbo_loglik.mean(), complexity, ent, log_prior(args, thetas) .mean(), log_jacobians.mean(), elbo_loglik_val.mean()
 
 
 # for given sample size and supposed lambda, learn resolution map g and return acheived ELBO (plus entropy)
@@ -139,19 +126,18 @@ def main():
 
     parser.add_argument('--prior_var', type=float, default=1e-1, metavar='N')
 
-    parser.add_argument('--lr', type=float, default=1e-3, metavar='N')
 
     parser.add_argument('--epochs', type=int, default=2000, metavar='N',
                         help='number of epochs to train (default: 200)')
-
     parser.add_argument('--batch_size', type=int, default=500, metavar='N',
                         help='input batch size for training (default: 100)')
+    parser.add_argument('--blundell_weighting', action='store_true')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='N')
+
 
     parser.add_argument('--nf',type=str,default='rnvp',choices=['iaf','rnvp','vanilla_rnvp'])
     parser.add_argument('--nf_hidden', type=int, default=16)
-
     parser.add_argument('--nf_layers', type=int, default=20)
-
     parser.add_argument('--nf_af', type=str, default='relu',choices=['relu','tanh'])
 
     parser.add_argument('--lmbda_star', type=float, default=40, metavar='N',
@@ -160,15 +146,15 @@ def main():
     parser.add_argument('--k', type=float, default=1, metavar='N',
                         help='?')
 
-    parser.add_argument('--path', type=str)
 
     parser.add_argument('--method', type=str, default='nf_gamma', choices=['nf_gamma','nf_gammatrunc','nf_gaussian','mf_gaussian'])
 
+    parser.add_argument('--nf_gamma_mode', type=str, default='icml')
+
     parser.add_argument('--display_interval',type=int, default=10)
 
-    parser.add_argument('--varparams_mode', type=str, default='icml')
 
-    parser.add_argument('--blundell_weighting', action='store_true')
+    parser.add_argument('--path', type=str)
 
     args = parser.parse_args()
 
@@ -177,43 +163,45 @@ def main():
     print(args.path)
     print('true rlct {}'.format(args.trueRLCT))
 
-    if args.method == 'nf_gamma' or args.method == 'nf_gaussian' or args.method == 'nf_gammatrunc':
+    if args.method == 'nf_gamma' or args.method == 'nf_gammatrunc':
 
-        if args.varparams_mode == 'abs_gauss':
+        if args.nf_gamma_mode == 'abs_gauss':
+
             args.lmbdas = 0.5*torch.ones(args.w_dim, 1)
             args.ks = torch.ones(args.w_dim, 1)
             args.betas = 0.5*torch.ones(args.w_dim, 1)
-        elif args.varparams_mode == 'abs_gauss_n':
-            lmbda_star = get_lmbda([args.H], args.dataset)[0]
-            args.lmbdas = 0.5*torch.ones(args.w_dim, 1)
-            args.lmbdas[0]=lmbda_star
-            args.ks = torch.ones(args.w_dim, 1)
-            args.betas = 0.5*torch.ones(args.w_dim, 1)
-            args.betas[0] = args.sample_size
-        elif args.varparams_mode == 'exp':
+
+        elif args.nf_gamma_mode == 'exp':
+
             args.lmbdas = torch.ones(args.w_dim, 1)
             args.ks = 0.5*torch.ones(args.w_dim, 1)
             args.betas = torch.ones(args.w_dim, 1)
-        elif args.varparams_mode == 'icml':
-            lmbda_star = get_lmbda([args.H], args.dataset)[0]
-            args.lmbdas = lmbda_star*torch.ones(args.w_dim, 1)
+
+        elif args.nf_gamma_mode == 'icml':
+
+            args.lmbdas = args.trueRLCT*torch.ones(args.w_dim, 1)
             args.ks = torch.ones(args.w_dim, 1)
-            args.betas = lmbda_star*torch.ones(args.w_dim, 1)
-            args.betas[0] = args.sample_size
-        elif args.varparams_mode == 'allones':
+            args.betas = args.trueRLCT*torch.ones(args.w_dim, 1)
+
+        elif args.nf_gamma_mode == 'allones':
+
             args.lmbdas = torch.ones(args.w_dim, 1)
             args.ks = torch.ones(args.w_dim, 1)
             args.betas = torch.ones(args.w_dim, 1)
+
+        args.lmbdas[0] = args.trueRLCT
+        args.betas[0] = args.sample_size
+
         args.hs = args.lmbdas * 2 * args.ks - 1
 
-        print(args)
+    print(args)
 
-        net, elbo_hist = train(args)
-        elbo, elbo_loglik, complexity, ent, logprior, log_jacobians, elbo_loglik_val = evaluate(net, args, R=100)
+    net, elbo_hist = train(args)
+    elbo, elbo_loglik, complexity, ent, logprior, log_jacobians, elbo_loglik_val = evaluate(net, args, R=100)
 
-        print('exact elbo {} plus entropy {} = {} for sample size n {}'.format(elbo, args.nSn, elbo+args.nSn, args.sample_size))
-        print('-lambda log n + (m-1) log log n: {}'.format(-args.trueRLCT*np.log(args.sample_size) + (args.truem-1.0)*np.log(np.log(args.sample_size))))
-        print('true lmbda {} versus supposed lmbda {}'.format(args.trueRLCT, args.lmbda_star))
+    print('exact elbo {} plus entropy {} = {} for sample size n {}'.format(elbo, args.nSn, elbo+args.nSn, args.sample_size))
+    print('-lambda log n + (m-1) log log n: {}'.format(-args.trueRLCT*np.log(args.sample_size) + (args.truem-1.0)*np.log(np.log(args.sample_size))))
+    print('true lmbda {} versus supposed lmbda {}'.format(args.trueRLCT, args.lmbda_star))
 
         # i = 0
         # metric = []
@@ -226,17 +214,17 @@ def main():
         # slope, intercept, r_value, p_value, std_err = stats.linregress(np.log(args.ns), metric)
         # print('est lmbda {} R2 {}'.format(-slope, r_value))
 
-    elif args.method == 'mf_gaussian':
-
-        # TODO: might be out of date, especially w.r.t. prior
-        print(args)
-        net = train_pyvarinf(args)
-        elbo, _, _ = evaluate_pyvarinf(net, args, R=10)
-
-        print('exact elbo {} plus entropy {} = {} for sample size n {}'.format(elbo, args.nSn, elbo+args.nSn, args.sample_size))
-        print('-lambda log n + (m-1) log log n: {}'.format(
-            -args.trueRLCT * np.log(args.sample_size) + (args.truem - 1.0) * np.log(np.log(args.sample_size))))
-        print('true lmbda {}'.format(args.trueRLCT))
+    # elif args.method == 'mf_gaussian':
+    #
+    #     # TODO: might be out of date, especially w.r.t. prior
+    #     print(args)
+    #     net = train_pyvarinf(args)
+    #     elbo, _, _ = evaluate_pyvarinf(net, args, R=10)
+    #
+    #     print('exact elbo {} plus entropy {} = {} for sample size n {}'.format(elbo, args.nSn, elbo+args.nSn, args.sample_size))
+    #     print('-lambda log n + (m-1) log log n: {}'.format(
+    #         -args.trueRLCT * np.log(args.sample_size) + (args.truem - 1.0) * np.log(np.log(args.sample_size))))
+    #     print('true lmbda {}'.format(args.trueRLCT))
 
     results_dict = {'elbo': elbo,
                     'asy_log_pDn': -args.trueRLCT * np.log(args.sample_size) + (args.truem - 1.0) * np.log(

utils.py
@@ -8,23 +8,35 @@ import torch.distributions as D
 # TODO: implement mixture prior, log uniform prior, horseshoe prior
 # evaluate log varphi(theta), returns vector
 def log_prior(args, thetas):
+    """
+
+    :param args:
+    :param thetas: [R, args.w_dim]
+    :return: returns log varphi(theta_1), ..., log varphi(theta_R)
+    """
 
     if args.prior == 'gaussian':
-    # varphi multivariate (dim=args.w_dim) Gaussian mean zero, covariance = diag(args.prior_var)
+
         return - args.w_dim/2*torch.log(2*torch.Tensor([np.pi])) \
                - (1/2)*args.w_dim*torch.log(torch.Tensor([args.prior_var])) \
                - torch.diag(torch.matmul(thetas,thetas.T))/(2*args.prior_var)
+
     elif args.prior == 'logunif':
-        a,b = 0.1 ,5
+
+        a,b = 0.1,5
         prob = (thetas*np.log(b/a))**(-1)
         return torch.log(prob)
+
     elif args.prior == 'gmm':
+
         # mix = D.Categorical(torch.ones(2, ))
         mix = D.Categorical(torch.Tensor([0.5, 0.5]))
         comp = D.Independent(D.Normal(torch.zeros(2, args.w_dim), torch.cat((1e-2*torch.ones(1,args.w_dim),torch.ones(1,args.w_dim)),0)), 1)
         gmm = D.MixtureSameFamily(mix, comp)
         return gmm.log_prob(thetas)
+
     elif args.prior == 'unif':
+
         return torch.log(1/(args.theta_upper-args.theta_lower)).sum() # assuming [-2,2]^d prior
 
 
@@ -60,7 +72,7 @@ def q_entropy_sample(args, xis):
 
     :param args:
     :param xis: R by w_dim
-    :return: estimate of E_q log q
+    :return: estimate of (scalar) E_q log q: 1/R \sum_{i=1}^R log q(xi_i)
     """
 
     if args.method == 'nf_gaussian':
@@ -105,7 +117,8 @@ def qj_gengamma_lognorm(h, k, beta, args):
     if args.method == 'nf_gamma':
         return G - torch.log(2*k) - lmbda*torch.log(beta)
     elif args.method == 'nf_gammatrunc':
-        return G - torch.log(2*k) - lmbda*torch.log(beta) + torch.log(torch.igamma(lmbda, beta*(args.xi_upper**(2*k))))
+        temp = beta * (args.xi_upper.unsqueeze(dim=1) ** (2 * k))
+        return G - torch.log(2*k) - lmbda*torch.log(beta) + torch.log(torch.igamma(lmbda, temp))
 
 
 # generate gamma(shape,rate)