0fa2bd771931df0d20aa8ce28cb8f883291c8ca9
Susan Wei <stawbygl@gmail.com>
2021-04-25 19:03:43 +1000

    K0 add training params

--- main.py
+++ main.py
@@ -30,10 +30,8 @@ def train(args):
             optimizer.zero_grad()
 
             if args.prior == 'unif':
-                R = 10
-            else:
-                R = 1
-            xis = sample_q(args, R, exact=True)  # [R, args.w_dim]
+                args.trainR = 10
+            xis = sample_q(args, args.trainR, exact=True)  # [R, args.w_dim]
 
             thetas, log_jacobians = resolution_network(xis)  # log_jacobians [R, 1]  E_q log |g'(xi)|
             args.theta_lower = torch.min(thetas, dim=0).values.detach()
@@ -57,12 +55,8 @@ def train(args):
             optimizer.step()
 
         if epoch % args.display_interval == 0:
-            if args.prior == 'unif':
-                R = 10
-            else:
-                R = 1
             elbo, elbo_loglik, complexity, ent, logprior, log_jacobians, elbo_loglik_val \
-                = evaluate(resolution_network, args, R)
+                = evaluate(resolution_network, args, R=10)
             print('epoch {}: loss {}, nSn {}, elbo {} '
                   '= loglik {} (loglik_val {}) - [complexity {} = qentropy {} - logprior {} - logjacob {}], '
                   .format(epoch, loss, args.nSn, elbo, elbo_loglik.mean(), elbo_loglik_val.mean(), complexity, ent, logprior.mean(), log_jacobians.mean()))
@@ -128,6 +122,7 @@ def main():
     parser.add_argument('--dataset', type=str, default='tanh',
                         help='dataset name from dataset_factory.py (default: )',
                         choices=['reducedrank', 'tanh','tanh_general'])
+    parser.add_argument('--zeromean', type=str)
     parser.add_argument('--H', type=int, default=1)
 
     parser.add_argument('--sample_size', type=int, default=5000,
@@ -140,6 +135,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=500)
     parser.add_argument('--blundell_weighting', action='store_true')
     parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--trainR', type=int, default = 1)
 
     parser.add_argument('--nf', type=str,default='rnvp', choices=['iaf','rnvp','vanilla_rnvp'])
     parser.add_argument('--nf_hidden', type=int, default=16)
